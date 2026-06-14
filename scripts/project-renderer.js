e2c90e41a65895015c60bf70d82c0f24e754f14c
Hong247 <cheokhong.design@gmail.com>
2026-06-14 15:03:02 -0700

    Apply project media overrides

--- scripts/project-renderer.js
+++ scripts/project-renderer.js
@@ -6,6 +6,7 @@ function renderProjectArchive() {
   var tbody = document.getElementById("projectArchive");
   var projects = window.PORTFOLIO_PROJECTS || [];
   var titleOverrides = window.PROJECT_TITLE_OVERRIDES || {};
+  var mediaOverrides = window.PROJECT_MEDIA_OVERRIDES || {};
 
   if (!tbody || !projects.length) {
     return;
@@ -16,22 +17,37 @@ function renderProjectArchive() {
   projects.forEach(function (project, index) {
     var number = String(index + 1).padStart(2, "0");
     var displayTitle = titleOverrides[project.id] || project.title;
+    var displayProject = applyProjectMediaOverride(project, mediaOverrides[project.id]);
     var headerRow = document.createElement("tr");
     var detailRow = document.createElement("tr");
 
     headerRow.className = "hover-trigger";
-    headerRow.setAttribute("data-image-source", project.preview || "");
+    headerRow.setAttribute("data-image-source", displayProject.preview || "");
     headerRow.innerHTML = '<td><button type="button" class="custom-btn" data-target="#' + project.id + '">' + number + '</button></td><td><button type="button" class="custom-btn" data-target="#' + project.id + '">' + displayTitle + '</button></td><td class="role-cell"><button type="button" class="custom-btn" data-target="#' + project.id + '">' + project.role + '</button></td><td><button type="button" class="custom-btn" data-target="#' + project.id + '">' + project.year + '</button></td>';
 
     detailRow.id = project.id;
     detailRow.className = "collapse";
-    detailRow.innerHTML = '<td colspan="4">' + buildProjectDetail(project) + '</td>';
+    detailRow.innerHTML = '<td colspan="4">' + buildProjectDetail(displayProject) + '</td>';
 
     tbody.appendChild(headerRow);
     tbody.appendChild(detailRow);
   });
 
   loadBase64ProjectImages(document);
+}
+
+function applyProjectMediaOverride(project, override) {
+  if (!override) {
+    return project;
+  }
+
+  var media = project.media || [];
+  var prependMedia = override.prependMedia || [];
+
+  return Object.assign({}, project, {
+    preview: override.preview || project.preview,
+    media: prependMedia.concat(media)
+  });
 }
 
 function buildProjectDetail(project) {
