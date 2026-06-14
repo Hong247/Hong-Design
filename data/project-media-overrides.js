6d97e4359985580982068c1ed11213bf117b4f8a
Hong247 <cheokhong.design@gmail.com>
2026-06-14 16:14:01 -0700

    Add final C Market website image overrides

--- data/project-media-overrides.js
+++ data/project-media-overrides.js
@@ -18,5 +18,45 @@ window.PROJECT_MEDIA_OVERRIDES = {
         alt: "C Market Coffee black tote bag editorial product photo"
       }
     ]
+  },
+  "demo-cmarket-site": {
+    preview: "images/C Market Coffee Website/cmarket website 1.jpg",
+    media: [
+      {
+        type: "image",
+        src: "images/C Market Coffee Website/cmarket website 1.jpg",
+        alt: "C Market Coffee website image 1"
+      },
+      {
+        type: "image",
+        src: "images/C Market Coffee Website/cmarket website 2.png",
+        alt: "C Market Coffee website image 2"
+      },
+      {
+        type: "image",
+        src: "images/C Market Coffee Website/cmarket website 3.png",
+        alt: "C Market Coffee website image 3"
+      },
+      {
+        type: "image",
+        src: "images/C Market Coffee Website/cmarket website 4.png",
+        alt: "C Market Coffee website image 4"
+      },
+      {
+        type: "image",
+        src: "images/C Market Coffee Website/cmarket website 5.png",
+        alt: "C Market Coffee website image 5"
+      },
+      {
+        type: "image",
+        src: "images/C Market Coffee Website/cmarket website 6.png",
+        alt: "C Market Coffee website image 6"
+      },
+      {
+        type: "image",
+        src: "images/C Market Coffee Website/cmarket website 7.png",
+        alt: "C Market Coffee website image 7"
+      }
+    ]
   }
 };
